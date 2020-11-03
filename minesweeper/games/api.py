794ee48c3809ec85f06c8d2e6cc1b522e277559b
Lenin Adin Hernández Vázquez <lennhv@gmail.com>
2020-11-02 22:35:33 -0600

    game api definition

---
+++ minesweeper/games/api.py
@@ -0,0 +1,26 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import (RetrieveUpdateAPIView,
+                                     ListCreateAPIView, UpdateAPIView)
+
+from .models import MinesweeperGame
+
+from .serializers import MinesweeperGameSerializer, MinesweeperGameUpdateSerializer, FlagSquareBoarSerializer
+
+
+class ListMinesweperApiView(ListCreateAPIView):
+    serializer_class = MinesweeperGameSerializer
+    queryset = MinesweeperGame.objects.all()
+    lookup_field = "id"
+
+
+class RetrieveUpdateDestroyApiView(RetrieveUpdateAPIView):
+    serializer_class = MinesweeperGameUpdateSerializer
+    queryset = MinesweeperGame.objects.all()
+    lookup_field = "id"
+
+
+class FlagSquareBoardApiView(UpdateAPIView):
+    serializer_class = FlagSquareBoarSerializer
+    queryset = MinesweeperGame.objects.all()
+    lookup_field = "id"
