0754230611c8176211c4c4a123b0f588b8d4814b
Lenin Adin Hernández Vázquez <lennhv@gmail.com>
2020-11-02 22:34:31 -0600

    minesweeper game model

--- minesweeper/users/models.py
+++ minesweeper/users/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .managers import UserManager
+
+
+class User(AbstractUser):
+
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.get_full_name()
