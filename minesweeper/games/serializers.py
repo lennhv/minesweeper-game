c27a313960eb1e851d056e2820bbe30652ae4fd1
Lenin Adin Hernández Vázquez <lennhv@gmail.com>
2020-11-03 23:17:19 -0600

    modify request inputs

--- minesweeper/games/serializers.py
+++ minesweeper/games/serializers.py
@@ -1,3 +1,4 @@
+import json
 from rest_framework import serializers
 from drf_yasg import openapi
 
@@ -5,18 +6,30 @@ from .models import MinesweeperGame
 
 
 class MinesweeperGameSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    board = serializers.SerializerMethodField(source='board')
 
     class Meta:
         model = MinesweeperGame
-        fields = ('id', 'board', 'rows', 'columns', 'mines',
+        fields = ('id', 'board', 'rows', 'columns', 'mines', 'user',
                   'start_at', 'finish_at', 'status', 'result',)
         read_only_fields = ('board', 'finish_at',
                             'start_at', 'status', 'result')
+
+    def create(self, validated_data):
+        return self.Meta.model.\
+            objects.create_game(**validated_data)
+
+    def get_board(self, obj):
+        if isinstance(obj.board, str):
+            return json.loads(obj.board)
+        return obj.board
 
 
 class SquareSerializer(serializers.ListField):
     """ Serializer to determine the location 
         of the square on the game board
+        [column, row]
     """
     max_length = 2
     min_length = 2
@@ -24,24 +37,44 @@ class SquareSerializer(serializers.ListField):
 
 
 class MinesweeperGameUpdateSerializer(serializers.ModelSerializer):
-
-    picked = SquareSerializer(write_only=True)
-    squares = serializers.ListField(
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    opens = serializers.ListField(
         child=SquareSerializer(write_only=True), min_length=1, write_only=True)
-    mine = serializers.BooleanField(write_only=True)
+    board = serializers.SerializerMethodField(source='board')
 
     class Meta:
         model = MinesweeperGame
-        fields = ('id', 'board', 'start_at', 'finish_at',
-                  'status', 'result', 'picked', 'squares', 'mine',)
+        fields = ('id', 'board', 'start_at', 'finish_at', 'user',
+                  'status', 'result', 'opens', )
         read_only_fields = ('board', 'finish_at',
                             'start_at', 'status', 'result')
 
+    def update(self, instance, validated_data):
+        return self.Meta.model.\
+            objects.update_game(**validated_data)
+
+    def get_board(self, obj):
+        if isinstance(obj.board, str):
+            return json.loads(obj.board)
+        return obj.board
+
 
 class FlagSquareBoarSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     square = SquareSerializer(write_only=True)
+    board = serializers.SerializerMethodField(source='board')
 
     class Meta:
         model = MinesweeperGame
-        fields = ('id', 'board', 'square')
+        fields = ('id', 'board', 'square', 'user',)
         read_only_fields = ('board', )
+
+    def update(self, instance, validated_data):
+        validated_data["instance"] = instance
+        return self.Meta.model.\
+            objects.flag_square(**validated_data)
+
+    def get_board(self, obj):
+        if isinstance(obj.board, str):
+            return json.loads(obj.board)
+        return obj.board
