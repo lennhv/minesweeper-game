4ee71d34e2f467519796bc4522cdd4e5c2bda174
Lenin Adin Hernández Vázquez <lennhv@gmail.com>
2020-11-04 20:20:51 -0600

    add games app admin

--- minesweeper/games/admin.py
+++ minesweeper/games/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import MinesweeperGame
+
+
+@admin.register(MinesweeperGame)
+class MinesweeperGameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'board', 'rows', 'columns',
+                    'mines', 'start_at', 'finish_at', 'status', 'result')
